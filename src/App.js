62b81ab6ba400bff9af727f78d76a710f4e0acd4
Lakindu Widuranga Alwis <29312088+MasterLWA@users.noreply.github.com>
2022-08-16 09:56:38 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+//import logo from './logo.svg';
+import './App.css';
+import Docs from './components/docs';
+
+function App() {
+  return (
+    <Docs />
+  );
+}
+
+export default App;
---
+++ src/components/docs.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Docs() {
+  return (
+    <div className='docs-main'>
+        <h1>docs</h1>
+
+        {/* add button*/}
+        <button className='add-docs'>
+            Add a Document
+        </button>
+    </div>
+  )
+}
